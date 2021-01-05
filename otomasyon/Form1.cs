a3edcc01dd6da92f85e9275d5ec2ac8ec57b1fef
cevherbakan <53389814+cevherbakan@users.noreply.github.com>
2021-01-05 12:37:22 +0300

    created

---
+++ otomasyon/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otomasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox txt = new TextBox();
+            txt.Text = "merhaba";
+            txt.Height = 60;
+            txt.Width = 120;
+            txt.Location = new System.Drawing.Point(200, 36);
+            txt.Clear();
+            this.Controls.Add(txt);
+            Form2 frm2 = new Form2();
+            frm2.MdiParent = this;
+            frm2.Show();
+            
+        }
+    }
+}
